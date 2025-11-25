7d510d0bde1f2980463173098bf32769057ab11c
anikettalwekar <talwekaraniket.11@gmail.com>
2025-11-25 17:20:01 +0530

    Fix EIP argument for new AWS provider

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -57,7 +57,7 @@ resource "aws_route_table_association" "public_assoc" {
 
 # Elastic IP for NAT
 resource "aws_eip" "nat_eip" {
-  vpc = true
+  domain = "vpc"
 
   tags = {
     Name = "${var.project_name}-nat-eip"
